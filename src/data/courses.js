9b44bdb37c1369da6354a87af8d54f5ac530e630
Diego Martins de Pinho <diegomartinsdepinho@msn.com>
2026-04-02 17:34:54 -0300

    fix: change python intro video

--- src/data/courses.js
+++ src/data/courses.js
@@ -11,7 +11,7 @@ export const courses = [
     prerequisites: [],
     workloadHours: 6,
     order: 2,
-    youtubeId: "XvmP4BC14D4",
+    youtubeId: "RNmaPJgN0U",
     ebook: {
       title: "Lógica de Programação com Python",
       siteName: "Diego Pinho - Educação e Tecnologia",
