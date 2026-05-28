0a10cfa801321131e194bea6b7306de7c8693ce4
Eduardo Rivas Vedooto <vedootoeduardo@gmail.com>
2026-05-28 17:04:39 -0300

    refactor: update styles across components to utilize design system variables and improve consistency

--- src/app/(home)/_components/Hero/Hero.jsx
+++ src/app/(home)/_components/Hero/Hero.jsx
@@ -2,9 +2,9 @@ import styles from './Hero.module.css';
 
 export default function Hero() {
   return (
-    <header className={styles.hero}>
+    <section className={styles.hero}>
       <h1 className={styles.hero__title}>Aprenda programação com quem ensina de verdade.</h1>
       <p className={styles.hero__subtitle}>Seu local de referência e confiança para aprender tecnologia.</p>
-    </header>
+    </section>
   );
 }
